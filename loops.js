29a3ee03f8fa809d2c3bf50ff5ded7d02c81c877
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-15 18:22:50 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,37 @@
+
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    
+    
+       if (i === 1){
+         array.push("I am 1 strange loop.");
+       }else{
+       array.push(`I am ${i} strange loops.`)
+         
+       }
+       
+  }
+ return array 
+  
+}
+
+
+
+function whileLoop(n){
+
+  while (n>0){
+  console.log(--n);
+  }
+  return "done";
+  
+}
+
+
+
+//console logs "I run once regardless." 1 time when passed an integer of 0 as a parameter.
+//console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.
+function doWhileLoop(num){
+  
+  
+  incrementVariable() < num`
+}
